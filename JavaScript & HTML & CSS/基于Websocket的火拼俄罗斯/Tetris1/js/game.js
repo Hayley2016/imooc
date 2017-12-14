b3fb263c8d65a879d06f22df5ed567820745b0a8
wanghaili <2826746721@qq.com>
2017-12-14 17:35:42 +0800

    基于Websocket的火拼俄罗斯

---
+++ JavaScript & HTML & CSS/基于Websocket的火拼俄罗斯/Tetris1/js/game.js
@@ -0,0 +1,204 @@
+var Game = function() {
+    /*定义Dom元素*/
+    var gameDiv;
+    var nextDiv;
+    /*游戏矩阵10*20*/
+    var gameData = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+    /*当前方块*/
+    var cur;
+    /*下一个方块*/
+    var next;
+    /*divs*/
+    var nextDivs = [];
+    var gameDivs = [];
+    /*初始化div*/
+    var initDiv = function(container, data, divs) {
+        for (var i = 0; i < data.length; i++) {
+            var div = [];
+            for (var j = 0; j < data[0].length; j++) {
+                var newNode = document.createElement('div');
+                newNode.className = 'none';
+                newNode.style.top = (i * 20) + 'px';
+                newNode.style.left = (j * 20) + 'px';
+                container.appendChild(newNode);
+                div.push(newNode);
+            }
+            divs.push(div);
+        }
+    };
+    /*刷新div*/
+    var refreshDiv = function(data, divs) {
+        for (var i = 0; i < data.length; i++) {
+            for (var j = 0; j < data[0].length; j++) {
+                var item = data[i][j];
+                if (item == 0) {
+                    divs[i][j].className = 'none';
+                } else if (item == 1) {
+                    divs[i][j].className = 'done';
+                } else if (item == 2) {
+                    divs[i][j].className = 'current';
+                }
+            }
+        }
+    };
+
+
+    /*监测点是否合法*/
+    var check = function(pos, x, y) {
+        if (pos.x + x < 0) {
+            return false;
+        } else if (pos.x + x >= gameData.length) {
+            return false;
+        } else if (pos.y + y < 0) {
+            return false;
+        } else if (pos.y + y >= gameData[0].length) {
+            return false;
+        } else if (gameData[pos.x + x][pos.y + y] == 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    /*检测数据是否合法*/
+    var isValid = function(pos, data) {
+        for (var i = 0; i < data.length; i++) {
+            for (var j = 0; j < data[0].length; j++) {
+                if (data[i][j] != 0) {
+                    if (!check(pos, i, j)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    /*清除数据*/
+    var clearData = function() {
+        for (var i = 0; i < cur.data.length; i++) {
+            for (var j = 0; j < cur.data[0].length; j++) {
+                if (check(cur.origin, i, j)) {
+                    gameData[cur.origin.x + i][cur.origin.y + j] = 0;
+                }
+            }
+        }
+    }
+    /*设置数据*/
+    var setData = function() {
+        for (var i = 0; i < cur.data.length; i++) {
+            for (var j = 0; j < cur.data[0].length; j++) {
+                if (check(cur.origin, i, j)) {
+                    gameData[cur.origin.x + i][cur.origin.y + j] = cur.data[i][j];
+                }
+            }
+        }
+    }
+    /*下移*/
+    var down = function() {
+        if (cur.canDown(isValid)) {
+            clearData();
+            cur.down();
+            setData();
+            refreshDiv(gameData, gameDivs);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /*左移*/
+    var left = function() {
+        if (cur.canLeft(isValid)) {
+            clearData();
+            cur.left();
+            setData();
+            refreshDiv(gameData, gameDivs);
+        }
+    }
+    /*右移*/
+    var right = function() {
+        if (cur.canRight(isValid)) {
+            clearData();
+            cur.right();
+            setData();
+            refreshDiv(gameData, gameDivs);
+        }
+    }
+    /*旋转*/
+    var rotate = function() {
+        if (cur.canRotate(isValid)) {
+            clearData();
+            cur.rotate();
+            setData();
+            refreshDiv(gameData, gameDivs);
+        }
+    }
+    /*方块移动到底部，给他固定*/
+    var fixed = function() {
+        for (var i = 0; i < cur.data.length; i++) {
+            for (var j = 0; j < cur.data[0].length; j++) {
+                if (check(cur.origin, i, j)) {
+                    if (gameData[cur.origin.x + i][cur.origin.y + j] == 2) {
+                        gameData[cur.origin.x + i][cur.origin.y + j] = 1;
+                    }
+                }
+            }
+        }
+        refreshDiv(gameData, gameDivs);
+    }
+    /*初始化*/
+    var init = function(doms) {
+        gameDiv = doms.gameDiv;
+        nextDiv = doms.nextDiv;
+        cur = SquareFactory.prototype.make(2, 2);
+        next = SquareFactory.prototype.make(3, 3);
+        initDiv(gameDiv, gameData, gameDivs);
+        initDiv(nextDiv, next.data, nextDivs);
+        setData();
+        refreshDiv(gameData, gameDivs);
+        refreshDiv(next.data, nextDivs);
+    }
+    /*使用下一个方块*/
+    var performNext = function(type, dir) {
+        cur = next;
+        setData();
+        next = SquareFactory.prototype.make(type, dir);
+        refreshDiv(gameData, gameDivs);
+        refreshDiv(next.data, nextDivs);
+    }
+    /*消行*/
+    var checkClear = function() {
+
+    }
+    /*导出API*/
+    this.init = init;
+    this.down = down;
+    this.left = left;
+    this.right = right;
+    this.rotate = rotate;
+    this.fall = function() {
+        while (down()) {}
+    };
+    this.fixed = fixed;
+    this.performNext = performNext;
+    this.checkClear = checkClear;
+}
---
+++ JavaScript & HTML & CSS/基于Websocket的火拼俄罗斯/Tetris1/js/local.js
@@ -0,0 +1,52 @@
+var Local = function() {
+    // 游戏对象
+    var game;
+    // 时间间隔
+    var INTERVAL = 200;
+    var timer = null;
+    // 绑定键盘事件
+    var bindKeyEvent = function() {
+        document.onkeydown = function(e) {
+            if (e.keyCode == 38) { // up
+                game.rotate();
+            } else if (e.keyCode == 39) { // right
+                game.right();
+            } else if (e.keyCode == 40) { // down
+                game.down();
+            } else if (e.keyCode == 37) { // left
+                game.left();
+            } else if (e.keyCode == 32) { //space
+                game.fall();
+            }
+        }
+    }
+    //移动
+    var move = function() {
+        if (!game.down()) {
+            game.fixed();
+            game.checkClear();
+            game.performNext(generateType(), generateDir());
+        }
+    }
+    //随机生成一个方块种类
+    var generateType = function() {
+        return Math.ceil(Math.random() * 7 - 1);
+    }
+    //随机生成一个旋转次数
+    var generateDir = function() {
+        return Math.ceil(Math.random() * 4 - 1);
+    }
+    // 开始
+    var start = function() {
+        var doms = {
+            nextDiv: document.getElementById('next'),
+            gameDiv: document.getElementById('game')
+        }
+        game = new Game();
+        game.init(doms);
+        bindKeyEvent();
+        timer = setInterval(move, INTERVAL);
+    }
+    // 导出API
+    this.start = start;
+}
