06ebe87e2e0e3bc64e56bc8d0cc0145ce2caf376
wanghaili <2826746721@qq.com>
2018-02-26 12:06:24 +0800

    nodejs学习

---
+++ Node.js/school/student.js
@@ -0,0 +1,4 @@
+function add(user) {
+    console.log('Add Student:' + user);
+}
+exports.add = add;
---
+++ Node.js/school/teacher.js
@@ -0,0 +1,4 @@
+function add(user) {
+    console.log('Add Teacher:' + user);
+}
+exports.add = add;
